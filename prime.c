d66c41860bb029d06008c3349a8f85417fbde08d
GGGWX <gggwx6198@yahoo.com>
2018-10-27 10:25:50 +0800

    simplify C-lang implementation for ASM

--- prime.c
+++ prime.c
@@ -3,15 +3,13 @@
 int main(int argc, char const *argv[])
 {
     for(int i = 2; i <= 100; i++){
-        int flag = 1;
-        for(int j = 2; j < i; j++){
+        for(int j = 2; j <= i; j++){
+            if(i == j){
+                printf(" %d ", j);
+            }
             if(i % j == 0){
-                flag = 0;
                 break;
             }
-        }
-        if(1 == flag){
-            printf(" %d ", i);
         }
     }
     return 0;
