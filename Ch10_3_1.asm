81192642a3761a80f450e88393f8f56b41eccab6
GGGWX <gggwx6198@yahoo.com>
2018-11-15 11:07:34 +0800

    assignment

--- Ch10_3_1.asm
+++ Ch10_3_1.asm
@@ -5,12 +5,8 @@ DATA ENDS
 CODE    SEGMENT
         ASSUME  CS:CODE,DS:DATA
         
-
 ;lower bound: 0V / 0.019V = 0
 ;upper bound: 5V / 0.019V = 262 > 2^8 -1 = 0FFH so upper bound = 00H
-;
-;
-;
 BEGIN:  MOV AL,0     ;lower bound
 UP:     OUT 220H,AL
         INC AL
@@ -23,8 +19,6 @@ DOWN:   OUT 220H,AL
         CMP AL,00H
         JNZ DOWN
         JMP BEGIN
-
-
 
 CODE    ENDS
 END     BEGIN
--- Ch10_3_2.asm
+++ Ch10_3_2.asm
@@ -5,12 +5,8 @@ DATA ENDS
 CODE    SEGMENT
         ASSUME  CS:CODE,DS:DATA
         
-
 ;lower bound: 1.2V / 0.019V = 61 = 3DH
 ;upper bound: 4V / 0.019V = 205 = 0CDH 
-;
-;
-;
 BEGIN:  MOV AL,3DH     ;lower bound
 UP:     OUT 220H,AL
         INC AL
